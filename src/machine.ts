438b752b60cf9934a5981ef5d626db498739711d
Segun Adebayo <joseshegs@gmail.com>
2023-01-15 19:56:00 +0400

    handing the focus events

--- src/machine.ts
+++ src/machine.ts
@@ -11,38 +11,50 @@ type MachineContext = {
   focusedIndex: number;
 };
 
-export const machine = createMachine<MachineContext, MachineState>({
-  id: "pin-input",
-  context: {
-    value: [],
-    focusedIndex: -1,
-  },
-  initial: "idle",
-  states: {
-    idle: {
-      on: {
-        FOCUS: {
-          target: "focused",
-          actions: ["setFocusedIndex"],
+export const machine = createMachine<MachineContext, MachineState>(
+  {
+    id: "pin-input",
+    context: {
+      value: [],
+      focusedIndex: -1,
+    },
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          FOCUS: {
+            target: "focused",
+            actions: ["setFocusedIndex"],
+          },
+        },
+      },
+      focused: {
+        on: {
+          BLUR: {
+            target: "idle",
+            actions: ["clearFocusedIndex"],
+          },
+          INPUT: {
+            actions: ["setFocusedValue", "focusNextInput"],
+          },
+          BACKSPACE: {
+            actions: ["clearFocusedValue", "focusPreviousInput"],
+          },
+          PASTE: {
+            actions: ["setPastedValue", "focusLastEmptyInput"],
+          },
         },
       },
     },
-    focused: {
-      on: {
-        BLUR: {
-          target: "idle",
-          actions: ["clearFocusedIndex"],
-        },
-        INPUT: {
-          actions: ["setFocusedValue", "focusNextInput"],
-        },
-        BACKSPACE: {
-          actions: ["clearFocusedValue", "focusPreviousInput"],
-        },
-        PASTE: {
-          actions: ["setPastedValue", "focusLastEmptyInput"],
-        },
+  },
+  {
+    actions: {
+      setFocusedIndex(context, event) {
+        context.focusedIndex = event.index;
+      },
+      clearFocusedIndex(context) {
+        context.focusedIndex = -1;
       },
     },
-  },
-});
+  }
+);
